a86c5326d021ffa1565363c2120f3300b3f6eac5
czykcinek <gorczakm@o2.pl>
2023-03-05 18:47:37 +0100

    Dodałem parę komentarzy i zmieniłem ustawienia wyświetlania

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 //Super Mariusz, gierka na zaliczenie projektu z przedmiotu In¿ynieria oprogramowania.
 
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
 
 using namespace sf;
 using namespace std;
 
 int main()
 {
-	RenderWindow window{ VideoMode{1280,720}, "Super_Mariusz" };
-	window.setFramerateLimit(144);
+	setlocale(LC_ALL, ""); //zachowanie polskiej czcionki w konsoli.
 
-	RectangleShape shape(Vector2f(300, 300));
+	RenderWindow window{ VideoMode{1280, 720, 32}, "Super_Mariusz" }; //Tworzenie okna o rozmiarach 1280x720px i nazwie Super_Mariusz.
+	window.setFramerateLimit(144); // Ustawienie limitu klatek na sekundê.
+
+	RectangleShape shape(Vector2f(300, 300)); // Tworzenie obiektu 
 	shape.setFillColor(Color::Blue);
 	
 	while (window.isOpen())
