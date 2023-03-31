0fffb366443fe16d337fb8973bb7e2075468ea70
ArturBA1 <UL0268740@edu.uni.lodz.pl>
2023-03-31 20:54:46 +0200

    Dodane nowe zmienne stałe

--- const.h
+++ const.h
@@ -1,8 +1,19 @@
 #pragma once
 //tutaj sta³e
 
+const unsigned char RozmiarKratki = 16;
+const unsigned char RozciaganieEkranu = 2;
 const int R = 100;
 const int SP = 55; //szerokosc prostokata
 const int WP = 44; //wysokosc prostokata
 const int W1 = 240; //szerokosc ekranu
 const int H1 = 256; //wysokosc	ekranu
+
+
+enum Cell	//nie wiem 
+{
+		Empty,
+		Wall
+};
+
+typedef std::vector < std::array<Cell, H1 / RozmiarKratki >> Map; //potrzebne do stworzenia mapy
